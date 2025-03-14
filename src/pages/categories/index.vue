bac3f6dcdc1a7aadfd0fd7b5c5bf40c1b310d704
Luke Edwards <lukeedwards30167116@gmail.com>
2025-03-14 22:33:53 +0000

    esnext build

--- src/pages/categories/index.vue
+++ src/pages/categories/index.vue
@@ -1,12 +1,4 @@
 <script setup lang="ts">
-import { useStore } from '@/store/store';
-import { useRoute } from 'vue-router';
-
-
-const route = useRoute()
-const { get_route } = useStore()
-
-const category = get_route(route.path)
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,12 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  build: {
+    target: "esnext",
+  },
 });
